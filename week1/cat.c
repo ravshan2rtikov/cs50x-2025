ee0ae39c61664bb80d882a703e13f718693241bc
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-10 12:41:26 +0700

    added small function in cat.c file

--- week1/cat.c
+++ week1/cat.c
@@ -1,11 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void meow(void);
+
 int main(void)
 {
   int i = 0;
   for (int i = 0; i < 3; i++)
   {
-    printf("meow\n");
+    meow();
   }
+}
+
+void meow(void)
+{
+  printf("meow\n");
 }
