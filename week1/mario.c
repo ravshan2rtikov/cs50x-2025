7b9473d13ff5070ac6a30e332f7de5fa2b2a1d5c
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-10 16:45:34 +0700

    the code prints the right half of the pyramid based on input

--- week1/mario.c
+++ week1/mario.c
@@ -1,18 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
+// prototypes
 void print_row(int bricks);
+// main
 int main(void)
 {
 
-// take input from the user
-    int height = get_int("What is the height of the pyramid?: ");
+// take input from the user + cascade the edge case with do loop
+    int height;
+    do
+    {
+        height = get_int("What is the height of the pyramid?: ");
+    } 
+    while (height < 1);
+    
 
 // write the pyramid code
-print_row(height);
+    for (int i = 0; i < height; i++)
+    {
+        print_row(i + 1);
+    }
 }
 
+// functions
 void print_row(int bricks)
 {
     for (int i = 0; i < bricks; i++)
