58f7f25ede96a2d84078e590f5e8298c116c4fab
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-11 15:22:53 +0700

    started week2, first file, score.c, to calc the avg

---
+++ week2/score.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int score1 = 77;
+    int score2 = 52;
+    int score3 = 33;
+    // print the average
+    printf("Average: %f.\n", (score1 + score2 + score3) / 3.0);
+}
