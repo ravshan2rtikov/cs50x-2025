863e05a10b95d46d3fb8810387b118c0219dc113
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-11 16:42:15 +0700

    added function to generalize the logic in length.c

--- week2/length.c
+++ week2/length.c
@@ -1,13 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_length (string s);
 int main(void)
 {
     string name = get_string("Name: ");
+    int length = get_length(name);
+    printf("%i\n", length);
+}
+
+int get_length (string s)
+{
     int n = 0;
-    while (name[n] != '\0')
+    while (s[n] != '\0')
     {
         n++;
     }
-    printf("%i\n", n);
+    return n;
 }
