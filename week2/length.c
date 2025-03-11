0bf49d41e4ab071be5e08c0ab5ab6c79e2098ffc
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-11 16:53:26 +0700

    added string.h library and used strlen function in length.c

--- week2/length.c
+++ week2/length.c
@@ -1,20 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-int get_length (string s);
 int main(void)
 {
     string name = get_string("Name: ");
-    int length = get_length(name);
+    int length = strlen(name);
     printf("%i\n", length);
-}
-
-int get_length (string s)
-{
-    int n = 0;
-    while (s[n] != '\0')
-    {
-        n++;
-    }
-    return n;
 }
