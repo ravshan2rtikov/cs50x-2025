d5d1ff7b07cba045450131de3cdbb6e4d7975949
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-11 20:38:34 +0700

    the code checks the string inpput for alphabetical order

--- week2/alphabet.c
+++ week2/alphabet.c
@@ -1,19 +1,34 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
+int main(int argc, string argv[])
 {
     // get user input
-    string text = get_string("Input: ");
+    if (argc != 2)
+    {
+        printf("Please provide a word.\n");
+        return 1;
+    }
+    string text = argv[1];
     // iterate through each element in the input string
     for (int i = 0; i < strlen(text); i++)
     {
-        if (text[i] < text[i - 1])
-        {           
-            printf("No\m");
+        if (!isalpha(text[i]))
+        {
+            printf("This is not a letter.\n");
+            return 2;
+        }
+    }
+    for (int i = 1, len = strlen(text); i < len; i++)
+    {
+        if (text[i] < text [i - 1])
+        {
+            printf("No\n");
             return 0;
         }
-        printf("Yes\n");
     }
+    printf("Yes\n");
+    
 }
