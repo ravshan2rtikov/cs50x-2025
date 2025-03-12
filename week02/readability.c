9acc7d33ab45ef3fa9e8f09063c2f9ea918a1381
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-12 07:20:29 +0700

    readability.c code is complete

--- week02/readability.c
+++ week02/readability.c
@@ -1,14 +1,14 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <math.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 // prototypes
-int count_letters (string text);
-int count_words (string text);
-int count_sentences (string text);
-float calc_grade (int letters, int words, int sentences);
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+float calc_grade(int letters, int words, int sentences);
 
 // main function
 int main(void)
@@ -17,7 +17,7 @@ int main(void)
     string text;
     do
     {
-    string text = get_string("Text: ");
+        text = get_string("Text: ");
     }
     while (text[0] == ' ');
 
@@ -30,11 +30,11 @@ int main(void)
     // print result
     if (grade < 1)
     {
-        printf("Before grade 1!\n");
+        printf("Below grade 1!");
     }
     else if (grade > 16)
     {
-        printf("Above grade 16!\n");
+        printf("Above grade 16!");
     }
     else
     {
@@ -44,26 +44,56 @@ int main(void)
 
 // functions
 // count letters
-int count_letters (string text)
+int count_letters(string text)
 {
-
+    int letters = 0;
+    int len = strlen(text);
+    for (int i = 0; i < len; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+    return letters;
 }
 
 // count words
-int count_words (string text)
+int count_words(string text)
 {
-
+    int words = 0;
+    int len = (strlen(text));
+    for (int i = 0; i < len; i++)
+    {
+        if (text[i] == ' ')
+        {
+            words++;
+        }
+    }
+    return words + 1;
 }
 
 // count sentences
-int count_sentences (string text)
+int count_sentences(string text)
 {
-
+    {
+        int sentences = 0;
+        int len = (strlen(text));
+        for (int i = 0; i < len; i++)
+        {
+            if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+            {
+                sentences++;
+            }
+        }
+        return sentences + 1;
+    }
 }
 
 // calculate grade
-float calc_grade (int letters, int words, int sentences)
+float calc_grade(int letters, int words, int sentences)
 {
-
+    float L = (letters / (float) words) * 100;
+    float S = (sentences / (float) words) * 100;
+    return 0.0588 * L - 0.296 * S - 15.8;
 }
-
