067baff9fb0b5273fb28eee179a01fc0734309ae
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-13 16:04:04 +0700

    election.c file creates a new struct, collects candidate names and votes and prints the candidate with highest votes

---
+++ week03/election.c
@@ -0,0 +1,36 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    string name;
+    int votes;
+} candidate;
+
+int main(void)
+{
+    const int num = 3;
+    candidate candidates[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        candidates[i].name = get_string("Name: ");
+        candidates[i].votes = get_int("Vote: ");
+    }
+
+    int highest_vote = 0;
+    for (int i = 0; i < num; i++)
+    {
+        if (candidates[i].votes > highest_vote)
+        highest_vote = candidates[i].votes;
+    }
+
+    for (int i =0; i < num; i++)
+    {
+        if (candidates[i].votes == highest_vote)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+}
