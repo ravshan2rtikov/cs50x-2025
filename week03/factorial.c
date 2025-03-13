b4ddd684c441c171285679c0e754ba7912c46e79
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-13 16:46:25 +0700

    factorial.c to calculate the factorial of input

---
+++ week03/factorial.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// prototypes
+int f(int n);
+// main function
+int main(void)
+{
+    int number;
+    do
+    {
+        number = get_init("Number: ");
+    }
+    while (number < 0);
+
+    int result = f(number);
+    printf("Factorial of %i is %i\n", number, result);
+}
+
+// functions
+int f(int n)
+{
+    // base case
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        // recursive case
+        return n * f(n - 1);
+    }
+}
