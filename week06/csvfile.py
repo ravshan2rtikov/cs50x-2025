7ab21445d5aa7f3467ac14e5bd9f4345e8c7b6cd
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-28 15:47:45 +0700

    week6 parctice, python csv module

--- week06/csvfile.py
+++ week06/csvfile.py
@@ -1,12 +1,8 @@
 import csv
-from os import close
 
-file = open("phonebook.csv", "a")
+with open("phonebook.csv", "a") as file:
+    name = input("Name: ")
+    number = input("Number: ")
 
-name = input("Name: ")
-number = input("Number: ")
-
-writer = csv.writer(file)
-writer.writerow([name, number])
-
-file.close()
+    writer = csv.writer(file)
+    writer.writerow([name, number])
