c55142e181998e2cc037820b3bc1484889dd475a
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-28 15:33:49 +0700

    week6 parctice, python dicts

---
+++ week06/phonebookdict.py
@@ -0,0 +1,8 @@
+people = {"yuliia": "617-495-1000", "david": "617-495-1000", "john": "949-468-2750"}
+
+name = input("Name: ")
+
+if name in people:
+    print(f"Number: {people[name]}")
+else:
+    print("not found")
